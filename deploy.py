e22d0e71a53c8e79736581f09e551447805a1142
Ertugrul Karademir <ekarademir@gmail.com>
2016-11-21 22:38:38 +0000

    Updated design

--- deploy.py
+++ deploy.py
@@ -14,7 +14,24 @@ def decorate(mdfile, templatefile = './template.thtml'):
     mdf = codecs.open(mdfile, 'r', 'utf-8')
     mdtext = mdf.read()
 
-    content = template.replace(u"{{CONTENT}}", markdown.markdown(mdtext), 1)
+    title = re.search('.+', mdtext).group()
+    title = re.sub('<.*?>', '', title)
+    title = re.sub('[^,\w\s]', '', title).strip()
+
+
+    content = template.replace(u"{{CONTENT}}", \
+            markdown.markdown(mdtext, \
+                extensions=['markdown.extensions.tables']), 1)
+
+    content = content.replace(u"{{TITLE}}", title)
+
+    templateparts = re.findall('\{\{.+?\.thtml\}\}',content)
+    for part in templateparts:
+        try:
+            with codecs.open(part[2:-2], mode='r', encoding='utf-8') as f:
+                content = content.replace(part, f.read())
+        except:
+            content = content.replace(part, '')
 
     # Second pass to correct .md links
     content = re.sub(r'href.*\.md', mdrepl, content)
@@ -41,7 +58,10 @@ def convertall():
 
     for mdfile in listdir('.'):
         if mdfile[-3:] == '.md' and mdfile not in ignore:
-            decorate(mdfile)
+            if mdfile[0:5] == 'essay':
+                decorate(mdfile, templatefile = './template.thtml')
+            else:
+                decorate(mdfile)
 
 def mdrepl(matchobj):
     s = matchobj.group(0)
